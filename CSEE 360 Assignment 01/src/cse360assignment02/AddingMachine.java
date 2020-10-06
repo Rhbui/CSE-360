5e7ac3d55e29d3e1bad3a8938b9ce0899f2d3109
ryanb <ryanb@DESKTOP-0DHHIN2>
2020-10-05 18:31:01 -0700

    Ryan Bui Assignment 01 Version Control

---
+++ CSEE 360 Assignment 01/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,40 @@
+package cse360assignment02;
+
+public class AddingMachine 
+{
+	  private int total;
+	  private String history;
+	  
+		  public AddingMachine () 
+		  {
+		    total = 0;  // not needed - included for clarity
+		    history = "0";
+		  }
+		  
+		  public int getTotal () 
+		  {
+		    return total;
+		  }
+		  
+		  public void add (int value) 
+		  {
+			  total += value;
+			  history += " + " + value;
+		  }
+	
+		  public void subtract (int value) 
+		  {
+			  total -= value;
+			  history += " - " + value;
+		  }
+	
+		  public String toString () 
+		  {
+		    return history;
+		  }
+	
+		  public void clear() 
+		  {
+			  total = 0;
+		  }
+}
